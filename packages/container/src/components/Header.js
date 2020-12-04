580ed7a2ef459a182e9154fa29351ac15536cce4
Mattias Mucherie <mattias.mattias.m@gmail.com>
2020-12-05 00:56:19 +0100

    Add routing

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import { Link as RouterLink } from 'react-router-dom';
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import { Link as RouterLink } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   '@global': {
@@ -52,16 +52,16 @@ const useStyles = makeStyles((theme) => ({
       paddingBottom: theme.spacing(6),
     },
   },
-}));
+}))
 
 export default function Header({ signedIn, onSignOut }) {
-  const classes = useStyles();
+  const classes = useStyles()
 
   const onClick = () => {
     if (signedIn && onSignOut) {
-      onSignOut();
+      onSignOut()
     }
-  };
+  }
 
   return (
     <React.Fragment>
@@ -94,5 +94,5 @@ export default function Header({ signedIn, onSignOut }) {
         </Toolbar>
       </AppBar>
     </React.Fragment>
-  );
+  )
 }
